d1c14c1d87cf18789268ae42bdf4258e34e2e9cb
jcanedo <jorge.canedo@gmail.com>
2023-10-08 22:51:45 +0200

    clase 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',// selector
+  templateUrl: './app.component.html', //plantilla
+  styleUrls: ['./app.component.css'] // archivo estilos
 })
 export class AppComponent {
   title = 'miPrimerApp';
+
+  saludo = "Hola alumnos de Angular";
 }
