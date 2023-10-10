b1b913918d30f9f408f108e9b2b2d52ac1838f8c
jcanedo <jorge.canedo@gmail.com>
2023-10-10 13:02:35 +0200

    clase 6

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  //template: "<p>Aquí iría un empleado</p>", // html en línea
   styleUrls: ['./empleado.component.css']
+  //styles:["p{background-color:red;}"] //estilos en línea
 })
 export class EmpleadoComponent {
 
